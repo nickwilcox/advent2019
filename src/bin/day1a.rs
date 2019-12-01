25e0e27737558d4a0195300bf013687eac358b25
Nick Wilcox <nick@nickwilcox.com>
2019-12-01 16:27:17 +1100

    day 1

---
+++ src/bin/day1a.rs
@@ -0,0 +1,19 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+fn fuel_for_mass(mass: u32) -> u32 {
+    mass / 3 - 2
+}
+
+pub fn main() {
+    let input_path = Path::new("input/1.txt");
+    let mut input_file = File::open(&input_path).unwrap();
+    let mut input = String::new();
+    input_file.read_to_string(&mut input).unwrap();
+    let result: u32 = input
+        .lines()
+        .map(|line| fuel_for_mass(line.parse::<u32>().unwrap()))
+        .sum();
+    println!("result = {}", result);
+}
